8101251af746166006fca017d3db72d5ba3f510c
tam.nguyen <tam.nguyen@paradox.ai>
2025-01-17 22:39:15 +0700

    update db

--- tasks/settings.py
+++ tasks/settings.py
@@ -80,8 +80,17 @@ WSGI_APPLICATION = "tasks.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django_api_testing",
+        "USER": "root",
+        "PASSWORD": "root1234",
+        "HOST": "localhost",
+        "PORT": 3306,
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET NAMES 'utf8mb4'",
+            "local_infile": True,
+        },
     }
 }
 
